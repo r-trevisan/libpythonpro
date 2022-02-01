c2ed82ce81279b7f5266bd3fb07f6900d94457bb
r-trevisan <rrhtt3@gmail.com>
2022-02-01 14:55:10 -0400

    Módulo Pytest Fixtures: Emulação BD. Implementação da Conexão, Sessão e Usuário. Isolamento de testes. Setup, tear down e escopos de fixtures. Arquivo Conftest

---
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -0,0 +1,14 @@
+from libpythonpro.spam.modelos import Usuario
+
+
+def test_salvar_usuario(sessao):
+    usuario = Usuario(nome='Rafael')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+
+
+def test_listar_usuarios(sessao):
+    usuarios = [Usuario(nome='Rafael'), (Usuario(nome='Henrique'))]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuarios == sessao.listar()
