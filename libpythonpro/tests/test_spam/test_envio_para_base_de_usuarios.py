564c49eee78b545aae2028ebec69100375ff7b52
r-trevisan <rrhtt3@gmail.com>
2022-02-02 15:18:09 -0400

    Módulo Injeção de Dependências: Produção de Código Testável, Injeção de Dependências, Mock, Módulo como Objeto, Isolamento de Imports e Biblioteca Pytest-Mock.

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,50 @@
+from unittest.mock import Mock
+
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+        Usuario(nome='Rafael', email='email1@gmail.com'),
+        Usuario(nome='Henrique', email='email2@gmail.com')
+        ],
+        [
+        Usuario(nome='Rafael', email='email1@gmail.com'),
+        ]
+    ]
+)
+def test_qtde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'email1@gmail.com',
         'Curso Python Pro',
         'Confira os Módulos Fantásticos'
     )
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+    def test_parametros_de_spam(sessao):
+        usuario=Usuario(nome='Rafael', email='email1@gmail.com')
+        sessao.salvar(usuario)
+        enviador = Mock()
+        enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+        enviador_de_spam.enviar_emails(
+            'email2@gmail.com',
+            'Curso Python Pro',
+            'Confira os Módulos Fantásticos'
+        )
+        enviador.enviar.assert_called_once_with(
+            'email2@gmail.com'
+            'email1@gmail.com'
+            'Curso Python Pro',
+            'Confira os Módulos Fantásticos'
+        )
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Rafael')
+    usuario = Usuario(nome='Rafael', email='email@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Rafael'), (Usuario(nome='Henrique'))]
+    usuarios = [
+        Usuario(nome='Rafael', email='email1@gmail.com'),
+        Usuario(nome='Henrique', email='email2@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
